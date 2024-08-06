04ecb4ae86068526ff6b2e11e999da6bd86356e3
Eklavya2003 <22052381@kiit.ac.in>
2024-08-06 14:02:04 +0530

    Database

--- src/university/management/system/Conn.java
+++ src/university/management/system/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
     Conn () {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "codeforinterview");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymangementsystem","root","Sujal@123");
             s = c.createStatement();
             
         } catch (Exception e) {
